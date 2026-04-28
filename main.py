70ad6c0347338c71bceecc72614c8b8bdd0981c2
matsu123abc <ak_matsuhashi@yahoo.co.jp>
2026-04-28 16:47:34 +0900

    update 1647

--- main.py
+++ main.py
@@ -280,6 +280,9 @@ async def ui():
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <title>買い物 栄養バランス分析ツール</title>
 
 <style>
